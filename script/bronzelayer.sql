e655b39b52f46d3289eb1a8c5708ffbcc435e44e
Priti Paudel Jaisi <140534761+Pritipaudel@users.noreply.github.com>
2026-03-30 17:23:03 +0545

    Update table definitions and data types in bronzelayer.sql
    Modified table definitions in bronzelayer.sql to drop existing tables and change data types from NUMERIC to INT for several columns.

--- script/bronzelayer.sql
+++ script/bronzelayer.sql
@@ -1,3 +1,4 @@
+drop table if exists bronze.crm_cust_info;
 create table bronze.crm_cust_info(
 cst_id INT,
 cst_key VARCHAR(50),
@@ -8,32 +9,47 @@ cst_gndr VARCHAR(50),
 cst_create_date DATE
 );
 
+drop table if exists bronze.crm_prd_info;
 create table bronze.crm_prd_info(
 prd_id INT,
 prd_key VARCHAR(50),
 prd_nm VARCHAR(50),
-prd_cost NUMERIC(10,2),
+prd_cost int,
 prd_line VARCHAR(50),
 prd_start_dt DATE,
 prd_end_dt DATE
 );
 
+drop table if exists bronze.crm_sales_details;
 create table bronze.crm_sales_details(
-sls_ord_num INT,
+sls_ord_num varchar(50),
 sls_prd_key VARCHAR(50),
 sls_cust_id INT,
-sls_order_dt DATE,
-sls_ship_dt DATE,
-sls_due_dt DATE,
-sls_sales NUMERIC(10,2),
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
 sls_quantity INT,
-sls_price NUMERIC(10,2)
+sls_price int
 );
 
+drop table if exists bronze.erp_CUST_AZ12;
 create table bronze.erp_CUST_AZ12(
 CID VARCHAR(50),
 BDATE DATE,
 GEN VARCHAR(50)
 );
 
---Data inserted manually in POSRGRESSSQL and I suggest you to create stored-procedure
+drop table if exists bronze.erp_LOC_A101;
+create table bronze.erp_LOC_A101(
+CID VARCHAR(50),
+CNTRY VARCHAR(50)
+);
+
+drop table if exists bronze.erp_PX_CAT_G1V2;
+create table bronze.erp_PX_CAT_G1V2(
+ID VARCHAR(50),
+CAT VARCHAR(50),
+SUBCAT VARCHAR(50),
+MAINTENANCE VARCHAR(50)
+);
